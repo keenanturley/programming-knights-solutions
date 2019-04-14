45741afcdb68ad4a97e26f6bb9adc1dfc058bc43
Keenan Turley <keenan.turley@gmail.com>
2019-04-14 18:33:49 -0400

    Add 6.5

--- 6/5.c
+++ 6/5.c
@@ -22,6 +22,15 @@ Practice Problem 6.5:
 
     (If you want to enhance the program you may output the nearest integer to 
     the actual value.) 
+
+Personal Note:
+    The problem says that a sphere with radius 2 would take up 8 cubic units
+    inside of the jar because it fits into a cube of that volume.
+
+    However, the volume of a sphere with radius 2 is 33.51 units.
+
+    The appropriate volume of a cube the sphere can fit into would be found
+    using the formula (2r)^3, which would make the volume of that cube 64.
 */
 
 #include <stdio.h>
@@ -31,6 +40,7 @@ int main(void)
 {
     float cylinder_radius, cylinder_height, cylinder_volume;
     float bean_radius, bean_volume;
+    int guess;
 
     /* Ask for cylinder details */
     printf("Cylinder radius: ");
@@ -42,11 +52,17 @@ int main(void)
     printf("Jellybean radius: ");
     scanf("%f", &bean_radius);
 
-    /* Calculate cylinder volume (V = pi * r^2 * h) */
+    /* Calculate cylinder volume: (V = pi * r^2 * h) */
     cylinder_volume = M_PI * pow(cylinder_radius, 2) * cylinder_height;
 
-    /* Calculate jellybean volume as a cube (2r)^3 */
+    /* Calculate jellybean volume as a cube: (2r)^3 */
+    bean_volume = pow((2 * bean_radius), 3);
 
+    /* Calculate guess to closest integer */
+    guess = round(cylinder_volume / bean_volume);
+
+    /* Output guess */
+    printf("Amount of jelly beans: %d\n", guess);
 
     return 0;
 }
